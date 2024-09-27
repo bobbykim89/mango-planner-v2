33ef3191b3d809c47db1621385ffce45d65d9825
Sihun Kim <skim585@asu.edu>
2024-09-27 12:26:34 -0700

    feat(plugins/*): replace init.global to plugin
    replace init.global middleware with init plugin, to prevent server side fetch on every page load.

--- middleware/init.global.ts
+++
@@ -1,6 +0,0 @@
-import { useInitPiniaStore } from '@/stores'
-
-export default defineNuxtRouteMiddleware(async () => {
-  const initPiniaStore = useInitPiniaStore()
-  await initPiniaStore.initStores()
-})
